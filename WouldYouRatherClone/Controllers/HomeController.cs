6c578b70800a37396ae1d241e950ea2d192e1c3c
Taiyyab <hafiztaiyyab11@gmail.com>
2017-03-26 18:40:37 -0500

    im cool

---
+++ WouldYouRatherClone/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WouldYouRatherClone.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int? id)
+        {
+            ViewBag.Title = "Home Page";
+            ViewBag.Id = id == null ? 1 : id;
+
+            return View();
+        }
+    }
+}
---
+++ WouldYouRatherClone/DatabaseScripts/Create_Table_Answers.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Answers] (
+    [Id]         INT            NOT NULL IDENTITY,
+    [QuestionId] INT            NOT NULL,
+    [AnswerText] NVARCHAR (256) NOT NULL,
+    [Score]      INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
